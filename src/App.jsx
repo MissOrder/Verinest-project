8942c9a7a1d61629e458389135563ce7d95a0b08
jessica mbah <jessicambah82@gmail.com>
2025-08-21 18:08:36 +0100

    verinest landing page and dashboard

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+//  import LandingPage from "./components/LandingPage";
+import Dashboard from "./components/Dashboard";
+export default function App() {
+  return (
+    <>
+    <Dashboard />
+      {/* <LandingPage /> */}
+    </>
+  );
+}
